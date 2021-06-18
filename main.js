5f4e2e5628ec625892c440dee1b16c73bb9ed45f
swapnanildas2008 <73822779+swapnanildas2008@users.noreply.github.com>
2021-06-18 13:02:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function dog(){
+  window.location = "/html/dog.html";
+}
+
+function bedroom(){
+  window.location = "/html/bedroom.html";
+}
+
+function bottle(){
+  window.location = "/html/bottle.html";
+}
+
+function fruits(){
+  window.location = "/html/fruits.html";
+}
+
+function tv(){
+  window.location = "/html/tv.html";
+}
